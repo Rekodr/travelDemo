3766ce261b7712c8ee620cfeeed0fdb470e4001e
rekodr <rekodr@mail.gvsu.edu>
2018-04-11 12:27:59 -0400

    fix

--- src/components/stepper/tripSummary.vue
+++ src/components/stepper/tripSummary.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <q-timeline>
-      <q-timeline-entry heading>Your trip plan in Tokyo</q-timeline-entry>
-      <q-timeline-entry color="red" :icon="activity.icon" v-for="activity in activities" :key="activity.time"
-        :title="activity.name" :subtitle="activity.time" side="left">
-        <div>
-          <q-item>
-            <q-item-main>{{activity.description}}</q-item-main>
-            <q-item-side :image="activity.picture"/>
-          </q-item>
-        </div>
-      </q-timeline-entry>
+      <div v-for="day in days" :key="day.id">
+        <q-timeline-entry heading>{{day.id}}</q-timeline-entry>
+        <q-timeline-entry color="red" :icon="activity.icon" v-for="activity in day.activities" :key="activity.time"
+          :title="activity.name" :subtitle="activity.time" side="left">
+          <div>
+            <q-item>
+              <q-item-main>{{activity.description}}</q-item-main>
+              <q-item-side :image="activity.picture"/>
+            </q-item>
+          </div>
+        </q-timeline-entry>
+      </div>
     </q-timeline>
   </div>
 </template>
@@ -20,20 +22,25 @@ export default {
   name: 'tripSummary',
   data () {
     return {
-      activities: [
+      days: [
         {
-          name: 'tea at the Yoko',
-          picture: 'assets/japanactivities/teaactivity.jpg',
-          time: '8:15 AM',
-          description: 'fun time',
-          icon: 'tag_faces'
-        },
-        {
-          name: 'Finshing dragon gods',
-          picture: 'assets/japanactivities/fishing.jpg',
-          time: '10:00 AM',
-          description: 'fun time',
-          icon: 'tag_faces'
+          id: 0,
+          activities: [
+            {
+              name: 'tea at the Yoko',
+              picture: 'assets/japanactivities/teaactivity.jpg',
+              time: '8:15 AM',
+              description: 'fun time',
+              icon: 'tag_faces'
+            },
+            {
+              name: 'Finshing dragon gods',
+              picture: 'assets/japanactivities/fishing.jpg',
+              time: '10:00 AM',
+              description: 'fun time',
+              icon: 'tag_faces'
+            }
+          ]
         }
       ]
     }
